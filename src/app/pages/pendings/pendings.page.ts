a0803a8004dcb21207f248c3224ab89508ef22d3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-27 18:19:10 -0500

    [+] Service > shopping-list: Implementa & despliega datos estaticos

--- src/app/pages/pendings/pendings.page.ts
+++ src/app/pages/pendings/pendings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingListService } from '../../services/shopping-list.service';   // Service
 
 @Component({
   selector: 'app-pendings',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PendingsPage implements OnInit {
 
-  constructor() { }
+  constructor( private _shoppingListService : ShoppingListService ) { }
 
   ngOnInit() {
   }
--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { List } from '../classes/List.model';       // Class Model
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
 
-  constructor() { }
+  /** Atributes */
+  lists : List[] = [];
+
+  constructor() {
+    let marketList = new List( 'Compras supermercado' ),
+        paymentList = new List( 'Pagos pendientes' ),
+        wishList = new List( 'Lista de deseos' );
+
+    console .log( 'ShoppingListService Inicializado' );
+
+    this .lists .push( marketList );
+    this .lists .push( paymentList );
+    this .lists .push( wishList );
+  }
+
 }
