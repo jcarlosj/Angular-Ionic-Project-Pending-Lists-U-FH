0adea289d34d9a8522286c3987e5920758bbc5f7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-29 10:50:57 -0500

    [+] Page > pending-add: Agrega items a la lista

---
+++ src/app/classes/Models.ts
@@ -0,0 +1,2 @@
+export { Item } from './Item.model';
+export { List } from './List.model';
--- src/app/pages/pendings/pending-add/pending-add.page.ts
+++ src/app/pages/pendings/pending-add/pending-add.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { List } from '../../../classes/Models';     // Models
 
 @Component({
   selector: 'app-pending-add',
@@ -9,11 +10,31 @@ export class PendingAddPage implements OnInit {
 
   /** Atributes */
   nameList: string;
-  nameItem: string;
+  nameItem: string = '';
+
+  items: List[] = [];
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add() {
+    console .log( 'Add item' );
+
+    /** Valida que los campos NO vengan vacios */
+    if( this .nameItem .length <= 0 ) {
+      console .log( 'No hay item' );
+      return;
+    }
+
+    let item = new List( this .nameItem );    // Instancia Clase & Asigna nombre del Item a la lista
+
+    this .items .push( item );                // Inserta item al Array
+    this .nameItem = '';                      // Limpia el campo
+    console .log( 'Lista', this .items );
+
   }
 
 }
