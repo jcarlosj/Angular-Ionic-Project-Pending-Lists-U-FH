da57073657c50b9919414edd2868f0e36a55d515
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-29 12:50:07 -0500

    [+] Page > pending-add: Agrega nombre a la lista

--- src/app/classes/Item.model.ts
+++ src/app/classes/Item.model.ts
@@ -1,4 +1,9 @@
 export class Item {
     name: string;
     isCompleted: boolean;
+
+    constructor( name: string ) {
+        this .name = name;
+        this .isCompleted = false;
+    }
 }
--- src/app/pages/pendings/pending-add/pending-add.page.ts
+++ src/app/pages/pendings/pending-add/pending-add.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { List } from '../../../classes/Models';     // Models
+import { AlertController } from '@ionic/angular';
+import { Item, List } from '../../../classes/Models';     // Models
 
 @Component({
   selector: 'app-pending-add',
@@ -9,32 +10,32 @@ import { List } from '../../../classes/Models';     // Models
 export class PendingAddPage implements OnInit {
 
   /** Atributes */
-  nameList: string;
+  nameList: string = '';
   nameItem: string = '';
 
-  items: List[] = [];
+  items: Item[] = [];
 
-
-  constructor() { }
+  constructor( public _alertController: AlertController ) { }
 
   ngOnInit() {
   }
 
   /** Agrega item a la lista */
   add() {
-    console .log( 'Add item' );
 
     /** Valida que los campos NO vengan vacios */
     if( this .nameItem .length <= 0 ) {
-      console .log( 'No hay item' );
+      console .log( 'Campo nombre del item vacio' );
       return;
     }
 
-    let item = new List( this .nameItem );    // Instancia Clase & Asigna nombre del Item a la lista
+    let item = new Item( this .nameItem );    // Instancia Clase & Asigna nombre del Item a la lista
+
+    console .log( 'Add item', this .nameItem );
 
     this .items .push( item );                // Inserta item al Array
     this .nameItem = '';                      // Limpia el campo
-    console .log( 'Lista', this .items );
+    console .log( 'Items', this .items );
 
   }
 
@@ -45,6 +46,31 @@ export class PendingAddPage implements OnInit {
     this .items .splice( idx, 1 );    /** Cambia el contanido de un array eliminando elemeto/s existentes (idx: indice donde inicia, 1: cantidad de campos a eliminar) */ 
 
     console .log( this .items );
+  }
+
+  async saveList() {
+    console .log( 'Save list' );
+
+    /** Valida que los campos NO vengan vacios */
+    if( this .nameList .length <= 0 ) {
+      console .log( 'Campo nombre de la lista vacio' );
+
+      const alert = await this ._alertController .create({
+        header: 'Atención',
+        subHeader: 'Dale nombre a tu lista',
+        message: 'El nombre permite asociar cada uno de los items a una lista específica',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+      return;
+    }
+
+    let list = new List( this .nameList );
+    list .items = this .items;      // Asigna la lista de items a la lista
+
+    console .log( 'List', list );
+
   }
 
 }
