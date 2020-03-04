488e611553df136395c7a95f811831579bf3e7b9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-04 16:46:55 -0500

    [+] CLI > pipe: pendings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PendingsPipe } from './pipes/pendings.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PendingsPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
---
+++ src/app/pipes/pendings.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PendingsPipe } from './pendings.pipe';
+
+describe('PendingsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PendingsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
